67f9ab5bba1a256e33d601a56af02682fca727fb
JEnriqueF <jesus.enrique.fernandezgonzalez@gmail.com>
2023-10-04 15:45:04 -0600

    ¨Primer commit

---
+++ Monitor/app/src/main/java/com/example/monitor/MainViewModel.java
@@ -0,0 +1,32 @@
+package com.example.monitor;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+
+import com.example.monitor.database.EqDatabase;
+
+import java.util.List;
+
+public class MainViewModel extends AndroidViewModel {
+    private final MutableLiveData<List<Earthquake>> eqList = new MutableLiveData<>();
+
+    public MainViewModel(@NonNull Application application) {
+        super(application);
+        EqDatabase database = EqDatabase.getDatabase((application));
+        repository = new MainRepository(database);
+    }
+
+    public LiveData<List<Earthquake>> getEqList(){
+        return repository.getEqList();
+    }
+
+    private MainRepository repository;
+
+    public void downloadEarthquakes(){
+        repository.descargarYGuardarEarthquakes();
+    }
+}
